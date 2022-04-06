81b261b17ef4f54523663f3bfa6543f37a21d403
harasatoki <s.hara@hajimari.inc>
2022-04-06 20:15:57 +0900

    ルート周り修正

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/hello', function () {
-    return view('welcome');
-});
+Route::get('/home', [HomeController::class,'index'])->name('home');
