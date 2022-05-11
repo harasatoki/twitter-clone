1b24679688c33ffc1e56e4b034ed072ab22a2288
harasatoki <s.hara@hajimari.inc>
2022-05-11 19:19:19 +0900

    favorite機能実装

---
+++ app/app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Favorite;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,Favorite $favorite)
+    {
+        $user = auth()->user();
+        $tweetId = $request->tweet_id;
+        $isFavorite = $favorite->isFavorite($user->id, $tweetId);
+
+        if(!$isFavorite) {
+            $favorite->storeFavorite($user->id, $tweetId);
+            return back();
+        }
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Favorite $favorite)
+    {
+        $userId = $favorite->user_id;
+        $tweetId = $favorite->tweet_id;
+        $favoriteId = $favorite->id;
+        $isFavorite = $favorite->isFavorite($userId, $tweetId);
+
+        if($isFavorite) {
+            $favorite->destroyFavorite($favoriteId);
+            return back();
+        }
+        return back();
+    }
+}
--- app/app/Models/Favorite.php
+++ app/app/Models/Favorite.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     public $timestamps = false;
+    public function isFavorite(Int $userId, Int $tweetId) 
+    {
+        return (boolean) $this->where('user_id', $userId)->where('tweet_id', $tweetId)->first();
+    }
+    public function storeFavorite(Int $userId, Int $tweetId)
+    {
+        $this->user_id = $userId;
+        $this->tweet_id = $tweetId;
+        $this->save();
+
+        return;
+    }
+    public function destroyFavorite(Int $favorite_id)
+    {
+        return $this->where('id', $favorite_id)->delete();
+    }
 }
